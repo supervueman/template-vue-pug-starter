d2359b5a1379da201aa20fa9893e0ebcc1c5b376
supervueman <supervueman@gmail.com>
2019-05-19 15:38:48 +0400

    Start template

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './routers';
+import {
+  store
+} from './store';
+import axios from 'axios';
+
+Vue.config.productionTip = false;
+const baseUrl =
+  process.env.NODE_ENV === 'development' ?
+  process.env.VUE_APP_SERVER_URL_DEV :
+  process.env.VUE_APP_SERVER_URL_PROD;
+
+axios.defaults.baseURL = baseUrl;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app')
